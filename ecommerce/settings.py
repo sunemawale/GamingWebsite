6a281cbddc14995328d88041f47f02b9eb6186ad
sunemawale <awaledon8@gmail.com>
2026-05-07 17:56:11 +0545

    Fix: Fixed vercel error

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a5vn9d@ss)xc+x%=ao)al141=oml4=dq6p&=vy*yu#s$84xv@1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
